a2b9cda3f034be6eedcd99931d41af7712fde164
Evan-Bru <nunuluck@gmail.com>
2022-07-21 19:33:28 -0300

    first commit

---
+++ src/test/java/dio/project/spring/ProjectSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package dio.project.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
